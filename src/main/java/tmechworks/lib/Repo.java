e31ce317a661947095689602903aaefd16689f4a
Johann Bernhardt <johann.bernhardt@tum.de>
2021-12-10 11:42:13 +0100

    Migrated to unified build script

--- src/main/java/tmechworks/lib/Repo.java
+++ src/main/java/tmechworks/lib/Repo.java
@@ -4,7 +4,7 @@ public final class Repo
 {
     public static final String modId = "TMechworks";
     public static final String modName = "Tinkers Mechworks";
-    public static final String modVer = "${version}";
+    public static final String modVer = "GRADLETOKEN_VERSION";
     public static final String modDeps = "required-after:Forge@[10.13.0.1208,];required-after:TConstruct;required-after:Mantle";
 
 }
